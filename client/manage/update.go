9edcdfd3abbf69ae4b1d8e28eb354141e27a1f38
sam bacha <sam@freighttrust.com>
2020-09-26 13:33:23 -0700

    update validator

---
+++ client/manage/update.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	// Create client
+	var kubeconfig string
+	kubeconfig, ok := os.LookupEnv("KUBECONFIG")
+	if !ok {
+		kubeconfig = filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	kubeclient := clientset.CoreV1().ServiceAccounts("besu")
+
+	// Create resource object
+	object := &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ServiceAccount",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "validator1-sa",
+			Namespace: "besu",
+		},
+	}
+
+	// Manage resource
+	_, err = kubeclient.Update(object)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ServiceAccount Updated successfully!")
+}
