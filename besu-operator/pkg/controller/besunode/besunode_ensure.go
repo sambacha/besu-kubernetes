f3d98879bd7780c6102dc21cd7a86627734c5e5a
sam bacha <sam@freighttrust.com>
2020-10-15 04:03:13 -0700

    Update besunode_ensure.go

--- besu-operator/pkg/controller/besunode/besunode_ensure.go
+++ besu-operator/pkg/controller/besunode/besunode_ensure.go
@@ -3,7 +3,7 @@ package besunode
 import (
 	"context"
 
-	hyperledgerv1alpha1 "github.com/Sumaid/besu-kubernetes/besu-operator/pkg/apis/hyperledger/v1alpha1"
+	hyperledgerv1alpha1 "github.com/sambacha/besu-kubernetes/besu-operator/pkg/apis/hyperledger/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
